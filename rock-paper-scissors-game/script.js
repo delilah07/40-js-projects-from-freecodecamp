25f69cda9b9d569cc4177797fee6c6f18bc3c4cb
unknown <you@example.com>
2022-01-20 16:24:24 +0100

    feat: add game over and pause

--- rock-paper-scissors-game/script.js
+++ rock-paper-scissors-game/script.js
@@ -31,96 +31,63 @@ const changeUser = () => {
   btns.forEach((btn) =>
     userStep ? btn.classList.remove("hide") : btn.classList.add("hide")
   );
+  console.log(userStep);
 };
 
-const compare = (userCh, compCh) => {
-  console.log("-----", userCh, compCh);
-  resultText.innerHTML = "";
-  if (userCh === compCh) {
-    console.log("same");
-    resultText.textContent = "It's a tie!";
-  } else {
-    if (userCh === "rock") {
-      if (compCh == "scissors") {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-    <span>${toUppCaseLetter(
-      userCh
-    )}</span> <sub>(user)</sub> bitts <span>${compCh}</span> <sub>(comp)</sub>. You win!`
-        );
-        console.log("win");
-        userScore++;
-        userScoreText.textContent = userScore;
-      } else {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-    <span>${toUppCaseLetter(
-      compCh
-    )}</span> <sub>(comp)</sub> covers <span>${userCh}</span> <sub>(user)</sub>. Computer wins!`
-        );
-        compScore++;
-        console.log("lose");
-        compScoreText.textContent = compScore;
-      }
-    }
-    if (userCh === "paper") {
-      if (compCh == "rock") {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-      <span>${toUppCaseLetter(
-        userCh
-      )}</span> <sub>(user)</sub> covers <span>${compCh}</span> <sub>(comp)</sub>. You win!`
-        );
-        userScore++;
-        console.log("win");
-        userScoreText.textContent = userScore;
-      } else {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-      <span>${toUppCaseLetter(
-        compCh
-      )}</span> <sub>(comp)</sub> cuts <span>${userCh}</span> <sub>(user)</sub>. Computer wins!`
-        );
-        compScore++;
-        compScoreText.textContent = compScore;
-        console.log("lose");
-      }
-    }
-    if (userCh === "scissors") {
-      if (compCh == "paper") {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-      <span>${toUppCaseLetter(
-        userCh
-      )}</span> <sub>(user)</sub> cuts <span>${compCh}</span> <sub>(comp)</sub>. You win!`
-        );
-        userScore++;
-        console.log("win");
-        userScoreText.textContent = userScore;
-      } else {
-        resultText.insertAdjacentHTML(
-          "afterbegin",
-          `
-      <span>${toUppCaseLetter(
-        compCh
-      )}</span> <sub>(comp)</sub> bitts <span>${userCh}</span> <sub>(user)</sub>. Computer wins!`
-        );
-        compScore++;
-        console.log("lose");
-        compScoreText.textContent = compScore;
-      }
-    }
-  }
-};
 const toUppCaseLetter = (str) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
+const textContent = (winCh, win, verb, loseCh, lose, congrat) => {
+  resultText.insertAdjacentHTML(
+    "afterbegin",
+    `<span>${toUppCaseLetter(
+      winCh
+    )}</span> <sub>(${win})</sub> ${verb} <span>${loseCh}</span> <sub>(${lose})</sub>. ${congrat}!`
+  );
+};
+
+const changeUserDivs = (win, verb, lose) => {
+  textContent(win, "user", verb, lose, "comp", "You win");
+  userScore += 1;
+  userScoreText.textContent = userScore;
+  console.log(userScore);
+  return userScore;
+};
+const changeCompDivs = (win, verb, lose) => {
+  textContent(win, "comp", verb, lose, "user", "Computer wins");
+  compScore += 1;
+  compScoreText.textContent = compScore;
+  console.log(compScore);
+  return compScore;
+};
+
+const compare = (userCh, compCh) => {
+  console.log("-----", userCh, compCh);
+  console.log("before", userScore, compScore);
+  resultText.innerHTML = "";
+
+  if (userCh === compCh) {
+    resultText.textContent = "It's a tie!";
+  } else {
+    if (userCh === "rock") {
+      compCh == "scissors"
+        ? changeUserDivs(userCh, "bitts", compCh)
+        : changeCompDivs(compCh, "covers", userCh);
+    }
+    if (userCh === "paper") {
+      compCh == "rock"
+        ? changeUserDivs(userCh, "covers", compCh)
+        : changeCompDivs(compCh, "cuts", userCh);
+    }
+    if (userCh === "scissors") {
+      compCh == "paper"
+        ? changeUserDivs(userCh, "cuts", compCh)
+        : changeCompDivs(compCh, "bitts", userCh);
+    }
+  }
+  console.log("after", userScore, compScore);
+};
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -129,10 +96,19 @@ btns.forEach((btn) =>
   btn.addEventListener("click", () => {
     const userChoise = btn.id;
     changeUser();
+    sleep(1500).then(() => {
+      compare(userChoise, compChoiseFunction());
 
-    compare(userChoise, compChoiseFunction());
-    sleep(1000).then(() => {
-      changeUser();
+      if (userScore === 3 || compScore === 3) {
+        resultText.textContent = `Game over. ${
+          compScore === 3 ? "Computer wins the game" : "You win the game"
+        }!!!`;
+        btns.forEach((btn) => (btn.disabled = true));
+        btns.forEach((btn) => btn.classList.add("hide"));
+        movementText.innerHTML = "";
+      } else {
+        changeUser();
+      }
     });
   })
 );
